8320dd19fa75e6dc7c53eacfa053b643bfcc4b33
Yuvraj <ymakode@falabella.cl>
2021-04-10 17:59:05 +0530

    json encode to base64

--- src/extension.ts
+++ src/extension.ts
@@ -8,17 +8,12 @@ import {
 	Range,
 	TextEdit,} from 'vscode';
 import * as prettier from 'prettier';
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+
+
 export function activate(context: ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "json-base64-vscode" is now active!');
+	console.log('Etension "json-base64-vscode" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let decodeBas64ToJsonDisposable = commands.registerCommand('json-base64-vscode.decodeBas64ToJson', async () => {
 		try {
 			const editor = window.activeTextEditor;
@@ -54,14 +49,52 @@ export function activate(context: ExtensionContext) {
 			}
 	});
 
-	let encodeJsonToBase64Disposable = commands.registerCommand('json-base64-vscode.encodeJsonToBase64', () => {
-		// The code you place here will be executed every time your command is executed
+	let encodeJsonToBase64Disposable = commands.registerCommand('json-base64-vscode.encodeJsonToBase64', async () => {
+		try {
+			const editor = window.activeTextEditor;
+			if (!editor) {
+				window.showInformationMessage('Not able to get text!');
+					return;
+			}
+			const document = editor.document;
+			const content: any = document.getText();
+			if (content === '') {
+				window.showInformationMessage('Text not available!');
+					return;
+			}
+			if (isValidJson(content)){
+				let buff = Buffer.from(content, 'utf8');
 
-		// Display a message box to the user
-		window.showInformationMessage('Hello World from encodeJsonToBase64!');
+				const lastLineId = document.lineCount - 1;
+				const range : Range = new Range(0, 0, lastLineId, document.lineAt(lastLineId).text.length);
+				const textEdit : TextEdit = TextEdit.replace(range, buff.toString('base64'));
+	
+				
+				await editor.edit((editBuilder) => {
+					editBuilder.replace(textEdit.range, textEdit.newText);
+				});
+
+				// Display a message box to the user
+				window.showInformationMessage('Successfully Encoded Base64 to Json');
+			} else {
+				window.showInformationMessage('Input text not a valid json');
+			} 
+		} catch (error) {
+			window.showErrorMessage(error.message);
+		} finally{
+		}
 	});
 	context.subscriptions.push(decodeBas64ToJsonDisposable, encodeJsonToBase64Disposable);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+function isValidJson(str : string) : boolean {
+    try {
+        JSON.parse(str)
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
