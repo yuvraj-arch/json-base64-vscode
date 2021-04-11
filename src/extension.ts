67552fa84175045650cf79d4fedda6c708c7b844
Yuvraj <ymakode@falabella.cl>
2021-04-11 23:51:11 +0530

    improve code fix bug

--- src/extension.ts
+++ src/extension.ts
@@ -6,21 +6,20 @@ import * as prettier from 'prettier';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	console.log('Etension "json-base64-vscode" is now active!');
-
+	console.log('Extension "Decoder/Decoder - Json/Base64" is now active!');
 
 	let decodeBas64ToJsonDisposable = vscode.commands.registerCommand('json-base64-vscode.decodeBas64ToJson', async () => {
 		try {
 			
 			const editor = vscode.window.activeTextEditor;
 			if (!editor) {
-				vscode.window.showInformationMessage('Not able to get text!');
+				vscode.window.showErrorMessage('Not able to get text!');
 					return;
 			}
 			const document = editor.document;
 			const content: any = document.getText();
 			if (content === '') {
-				vscode.window.showInformationMessage('Text not available!');
+				vscode.window.showInformationMessage('Error:: Text not available!');
 					return;
 			}
 			let buff = Buffer.from(content, 'base64');
@@ -39,9 +38,9 @@ export function activate(context: vscode.ExtensionContext) {
 			vscode.languages.setTextDocumentLanguage(editor.document, 'json');
 			// Display a message box to the user
 			vscode.window.showInformationMessage('Successfully Decoded Base64 to Json');
+			goToStartingPosition(editor)
 			} catch (error) {
 				vscode.window.showErrorMessage(error.message);
-			} finally{
 			}
 	});
 
@@ -56,35 +55,35 @@ export function activate(context: vscode.ExtensionContext) {
 			const document = editor.document;
 			const content: any = document.getText();
 			if (content === '') {
-				vscode.window.showInformationMessage('Text not available!');
+				vscode.window.showErrorMessage('Error:: Text not available!');
 					return;
 			}
-			if (isValidJson(content)){
-				let buff = Buffer.from(content, 'utf8');
+			let buff : Buffer;
+			try {
+					buff = Buffer.from(JSON.stringify(JSON.parse(content)), 'utf8');
+			} catch (error) {
+				vscode.window.showErrorMessage('Error:: Input text not a valid json');
+				return;
+			}
 
-				const lastLineId = document.lineCount - 1;
-				const range : vscode.Range = new vscode.Range(0, 0, lastLineId, document.lineAt(lastLineId).text.length);
-				const textEdit : vscode.TextEdit = vscode.TextEdit.replace(range, buff.toString('base64'));
+			const lastLineId = document.lineCount - 1;
+			const range : vscode.Range = new vscode.Range(0, 0, lastLineId, document.lineAt(lastLineId).text.length);
+			const textEdit : vscode.TextEdit = vscode.TextEdit.replace(range, buff.toString('base64'));
 	
-				
-				await editor.edit((editBuilder) => {
-					editBuilder.replace(textEdit.range, textEdit.newText);
-				});
+			await editor.edit((editBuilder) => {
+				editBuilder.replace(textEdit.range, textEdit.newText);
+			});
 
-
-				/*const wordWrap = vscode.workspace.getConfiguration('', document).get('editor.wordWrap');
-				if('off' === wordWrap) {
-					vscode.commands.executeCommand('editor.action.toggleWordWrap');
-				}*/
-				// Display a message box to the user
-				vscode.languages.setTextDocumentLanguage(editor.document, 'plaintext');
-				vscode.window.showInformationMessage('Successfully Encoded Base64 to Json');
-			} else {
-				vscode.window.showInformationMessage('Input text not a valid json');
-			} 
+			/*const wordWrap = vscode.workspace.getConfiguration('', document).get('editor.wordWrap');
+			if('off' === wordWrap) {
+				vscode.commands.executeCommand('editor.action.toggleWordWrap');
+			}*/
+			// Display a message box to the user
+			vscode.languages.setTextDocumentLanguage(editor.document, 'plaintext');
+			vscode.window.showInformationMessage('Successfully Encoded Base64 to Json');
+			goToStartingPosition(editor)
 		} catch (error) {
 			vscode.window.showErrorMessage(error.message);
-		} finally{
 		}
 	});
 	context.subscriptions.push(decodeBas64ToJsonDisposable, encodeJsonToBase64Disposable);
@@ -93,11 +92,10 @@ export function activate(context: vscode.ExtensionContext) {
 // this method is called when your extension is deactivated
 export function deactivate() {}
 
-function isValidJson(str : string) : boolean {
-    try {
-        JSON.parse(str)
-        return true;
-    } catch (e) {
-        return false;
-    }
+function goToStartingPosition(textEditor: vscode.TextEditor) : void {
+    if (!textEditor) return;
+    const position = new vscode.Position(0, 0);
+    textEditor.selection = new vscode.Selection(position, position);
+    textEditor.revealRange(new vscode.Range(position, position), vscode.TextEditorRevealType.Default);
 }
+
